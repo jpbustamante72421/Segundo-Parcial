bc67f39c186a26ea22b1826005117bc4aa350c1a
jpbustamante72421 <72421leo@universidaddeleon.edu.mx>
2026-03-09 09:14:38 -0600

    09/03/2026

--- cinepolis/cine.py
+++ cinepolis/cine.py
@@ -4,49 +4,41 @@ from PIL import Image, ImageTk
 
 def cinepolis():
     try:
-        nombre = entry_nombre.get()
-        personas = int(entry_personas.get())
-        cantidad_boletos = int(entry_boleto.get())
-        cineco = variable_cinec.get()
-
-        precio = 12
-        venta_max = personas * 7
-
-        if nombre == "":
+        nombre=entry_nombre.get()
+        personas= int(entry_personas.get())
+        cantidad_boletos=int(entry_boleto.get())
+        cineco=variable_cinec.get()        
+        precio=12
+        venta_max=personas * 7
+        if nombre=="":
             messagebox.showerror("Error", "Ingresa el nombre de la persona.")
             return
-
-        if personas <= 0:
+        if personas<=0:
             messagebox.showerror("Error", "Debe haber al menos una persona.")
             return
-
-        if cantidad_boletos <= 0:
+        if cantidad_boletos<=0:
             messagebox.showerror("Error", "Debe comprar al menos un boleto.")
             return
-
-        if cantidad_boletos > venta_max:
+        if cantidad_boletos>venta_max:
             messagebox.showerror("Error", f"Máximo permitido: {venta_max} boletos.")
             return
-
-        total = cantidad_boletos * precio
+        total= cantidad_boletos*precio
 
         # Descuento por cantidad de boletos
-        if cantidad_boletos > 5:
-            descuento = total * 0.15
-        elif cantidad_boletos >= 3:
-            descuento = total * 0.10
+        if cantidad_boletos> 5:
+            descuento= total * 0.15
+        elif cantidad_boletos>= 3:
+            descuento= total * 0.10
         else:
             descuento = 0
-
         subtotal = total - descuento
 
         # Descuento CINECO
-        if cineco == 1:
-            descuento_cineco = subtotal * 0.10
+        if cineco== 1:
+            descuento_cineco =subtotal * 0.10
         else:
-            descuento_cineco = 0
-
-        total_pagar = subtotal - descuento_cineco
+            descuento_cineco =0
+        total_pagar= subtotal-descuento_cineco
 
         resultado = f"""
         Nombre: {nombre}
@@ -68,12 +60,10 @@ ventana = tk.Tk()
 ventana.title("Cinépolis")
 ventana.geometry("1000x600")
 ventana.resizable(False, False)
-
 # imagen fond
 imagen = Image.open("CINEpol.png")
 imagen = imagen.resize((1000, 600))
 fondo = ImageTk.PhotoImage(imagen)
-
 label_fondo = tk.Label(ventana, image=fondo)
 label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -95,7 +85,6 @@ entry_boleto.place(x=200, y=140, width=160)
 
 variable_cinec = tk.IntVar()
 tk.Checkbutton(frame_izquierda,text="Descuento tarjeta CINECO",variable=variable_cinec,bg="#22478D",fg="white",selectcolor="#22478D").place(x=90, y=180)
-
 #DERECHO
 frame_derecha = tk.Frame(ventana, bg="#22478D", bd=5)
 frame_derecha.place(x=600, y=220, width=250, height=150)
